4af8e23fa2c38d5d0dd170e05ebcfa14893c1def
Rami1125 <rami.msarwa1@gmail.com>
2025-07-30 08:28:33 +0300

    Update data.js

--- data.js
+++ data.js
@@ -1,67 +1,68 @@
-// נתוני מוצרים לדוגמה
+// נתוני מוצרים לדוגמה (לשימוש כפאלבק אם שליפה מגיליון נכשלת)
+// הערה: המחירים הוסרו מכאן בהתאם לבקשתך.
 export const productsData = {
     "חשמל": [
-        { id: "e1", name: "מנעול חכם Linus® L2 מבית YALE!", description: "פתיחה אוטומטית ללא מפתח - עם קודן או טביעת אצבע.", image: "https://gw-assets.assaabloy.com/is/image/assaabloy/5052847131336.PT03-20?wid=1520", price: 850 },
-        { id: "e2", name: "שקע כפול גוויס", description: "שקע חשמל כפול איכותי להתקנה בקיר.", image: "https://d3m9l0v76dty0.cloudfront.net/system/photos/14257848/large/75048cebc446ce5d37f46fa818aa00f5.jpeg", price: 45 },
-        { id: "e3", name: "מפסק תאורה", description: "מפסק לדוד חשמל, תאורה ועוד.", image: "https://d3m9l0v76dty0.cloudfront.net/system/photos/6518728/large/8b8660dd841cce0f1c43f63d324f2871.jpg", price: 30 },
-        { id: "e4", name: "לוח חשמל 12 מקום", description: "לוח חשמל תקני לדירות ומשרדים קטנים.", image: "https://d3m9l0v76dty0.cloudfront.net/system/photos/13898653/large/51b09d54003405406700bcd5890948a9.jpg", price: 280 },
-        { id: "e5", name: "נורות לד חסכוניות", description: "מארז 5 נורות לד E27 חסכוניות באנרגיה.", image: "https://d3m9l0v76dty0.cloudfront.net/system/photos/7199759/extra_large/248ac70909c5ffd31474fa1625887625.jpg", price: 75 },
-        { id: "e6", name: "פס צבירה לתאורה", description: "פס צבירה דקורטיבי לתאורת ספוטים.", image: "https://www.peleg4home.com/wp-content/uploads/2022/02/4.png", price: 120 },
-        { id: "e7", name: "מאוורר תקרה", description: "מאוורר תקרה מעוצב עם שלט רחוק.", image: "https://ksp.co.il/shop/items/512/285521.jpg", price: 550 },
-        { id: "e8", name: "גוף תאורה צמוד תקרה", description: "גוף תאורה לד עגול, חסכוני באנרגיה.", image: "https://dealteora.co.il/cdn/shop/products/S-24061-600R-2700-BK.jpg?v=1647540262&width=800", price: 180 }
+        { id: "e1", name: "מנעול חכם Linus® L2 מבית YALE!", description: "פתיחה אוטומטית ללא מפתח - עם קודן או טביעת אצבע.", image: "https://gw-assets.assaabloy.com/is/image/assaabloy/5052847131336.PT03-20?wid=1520" },
+        { id: "e2", name: "שקע כפול גוויס", description: "שקע חשמל כפול איכותי להתקנה בקיר.", image: "https://d3m9l0v76dty0.cloudfront.net/system/photos/14257848/large/75048cebc446ce5d37f46fa818aa00f5.jpeg" },
+        { id: "e3", name: "מפסק תאורה", description: "מפסק לדוד חשמל, תאורה ועוד.", image: "https://d3m9l0v76dty0.cloudfront.net/system/photos/6518728/large/8b8660dd841cce0f1c43f63d324f2871.jpg" },
+        { id: "e4", name: "לוח חשמל 12 מקום", description: "לוח חשמל תקני לדירות ומשרדים קטנים.", image: "https://d3m9l0v76dty0.cloudfront.net/system/photos/13898653/large/51b09d54003405406700bcd5890948a9.jpg" },
+        { id: "e5", name: "נורות לד חסכוניות", description: "מארז 5 נורות לד E27 חסכוניות באנרגיה.", image: "https://d3m9l0v76dty0.cloudfront.net/system/photos/7199759/extra_large/248ac70909c5ffd31474fa1625887625.jpg" },
+        { id: "e6", name: "פס צבירה לתאורה", description: "פס צבירה דקורטיבי לתאורת ספוטים.", image: "https://www.peleg4home.com/wp-content/uploads/2022/02/4.png" },
+        { id: "e7", name: "מאוורר תקרה", description: "מאוורר תקרה מעוצב עם שלט רחוק.", image: "https://ksp.co.il/shop/items/512/285521.jpg" },
+        { id: "e8", name: "גוף תאורה צמוד תקרה", description: "גוף תאורה לד עגול, חסכוני באנרגיה.", image: "https://dealteora.co.il/cdn/shop/products/S-24061-600R-2700-BK.jpg?v=1647540262&width=800" }
     ],
     "חומרי בניין": [
-        { id: "b1", name: "בטון מוכן 25 ק\"ג", description: "שק בטון מוכן איכותי לבנייה.", image: "https://marzevit.co.il/wp-content/uploads/2020/05/fe84e8babcbd6108c37de4084610afa1.jpg", price: 35 },
-        { id: "b2", name: "חול בלה (חצי קוב)", description: "בלה חול ים זיפזיף שק ענק.", image: "https://probuilder.co.il/wp-content/uploads/2021/11/%D7%91%D7%9C%D7%94-%D7%97%D7%95%D7%9C-%D7%99%D7%9D-%D7%96%D7%99%D7%A4%D7%96%D7%99%D7%A3-%D7%A9%D7%A7-%D7%A2%D7%A0%D7%A7.png", price: 150 },
-        { id: "b3", name: "בלוק פומיס", description: "בלוק פומיס קל ואיכותי לבניית קירות.", image: "https://probuilder.co.il/wp-content/uploads/2025/01/%D7%91%D7%9C%D7%95%D7%A7-%D7%A4%D7%95%D7%9E%D7%99%D7%A1-%D7%91%D7%A0%D7%99%D7%99%D7%94-%D7%9E%D7%91%D7%98%D7%95%D7%9F-%D7%98%D7%A8%D7%9E%D7%95%D7%93%D7%9F.png", price: 8 },
-        { id: "b4", name: "מוצרי איטום סיקה", description: "מוצרי איטום של סיקה - מגוון פתרונות.", image: "https://better4u.co.il/wp-content/uploads/2021/01/sika-logo.png", price: 120 },
-        { id: "b5", name: "מלט 50 ק\"ג", description: "שק מלט פורטלנד איכותי לבנייה.", image: "https://www.sika.co.il/content/dam/dms/il01/k/sika-cem-grout-212-il.jpg", price: 40 },
-        { id: "b6", name: "טיח גבס", description: "טיח גבס מוכן לשימוש, קל ליישום.", image: "https://www.israbuild.co.il/wp-content/uploads/2021/03/gypsum-plaster.jpg", price: 60 },
-        { id: "b7", name: "לוחות גבס", description: "לוחות גבס לבניית קירות ותקרות.", image: "https://www.israbuild.co.il/wp-content/uploads/2021/03/gypsum-board.jpg", price: 70 },
-        { id: "b8", name: "דבק קרמיקה", description: "דבק גמיש לריצוף וחיפוי קרמיקה.", image: "https://www.sika.co.il/content/dam/dms/il01/k/sika-tilofix-il.jpg", price: 55 }
+        { id: "b1", name: "בטון מוכן 25 ק\"ג", description: "שק בטון מוכן איכותי לבנייה.", image: "https://marzevit.co.il/wp-content/uploads/2020/05/fe84e8babcbd6108c37de4084610afa1.jpg" },
+        { id: "b2", name: "חול בלה (חצי קוב)", description: "בלה חול ים זיפזיף שק ענק.", image: "https://probuilder.co.il/wp-content/uploads/2021/11/%D7%91%D7%9C%D7%94-%D7%97%D7%95%D7%9C-%D7%99%D7%9D-%D7%96%D7%99%D7%A4%D7%96%D7%99%D7%A3-%D7%A9%D7%A7-%D7%A2%D7%A0%D7%A7.png" },
+        { id: "b3", name: "בלוק פומיס", description: "בלוק פומיס קל ואיכותי לבניית קירות.", image: "https://probuilder.co.il/wp-content/uploads/2025/01/%D7%91%D7%9C%D7%95%D7%A7-%D7%A4%D7%95%D7%9E%D7%99%D7%A1-%D7%91%D7%A0%D7%99%D7%99%D7%94-%D7%9E%D7%91%D7%98%D7%95%D7%9F-%D7%98%D7%A8%D7%9E%D7%95%D7%93%D7%9F.png" },
+        { id: "b4", name: "מוצרי איטום סיקה", description: "מוצרי איטום של סיקה - מגוון פתרונות.", image: "https://better4u.co.il/wp-content/uploads/2021/01/sika-logo.png" },
+        { id: "b5", name: "מלט 50 ק\"ג", description: "שק מלט פורטלנד איכותי לבנייה.", image: "https://www.sika.co.il/content/dam/dms/il01/k/sika-cem-grout-212-il.jpg" },
+        { id: "b6", name: "טיח גבס", description: "טיח גבס מוכן לשימוש, קל ליישום.", image: "https://www.israbuild.co.il/wp-content/uploads/2021/03/gypsum-plaster.jpg" },
+        { id: "b7", name: "לוחות גבס", description: "לוחות גבס לבניית קירות ותקרות.", image: "https://www.israbuild.co.il/wp-content/uploads/2021/03/gypsum-board.jpg" },
+        { id: "b8", name: "דבק קרמיקה", description: "דבק גמיש לריצוף וחיפוי קרמיקה.", image: "https://www.sika.co.il/content/dam/dms/il01/k/sika-tilofix-il.jpg" }
     ],
     "כלי עבודה": [
-        { id: "t1", name: "פטיש 5 ק\"ג", description: "פטיש כבד לעבודות הריסה ובנייה.", image: "https://zurmarket.co.il/cdn/shop/files/51fLQVbi5EL._AC_SL1500.jpg?v=1749121553&width=1946", price: 90 },
-        { id: "t2", name: "מקדחה רוטטת", description: "מקדחה מקצועית לקידוח בבטון ועץ.", image: "https://d3m9l0v76dty0.cloudfront.net/system/photos/11268082/large/44ba92ae69c8e38578d169a71a247471.webp", price: 320 },
-        { id: "t3", name: "מפתח שוודי", description: "מפתח שוודי מתכוונן בגדלים שונים.", image: "https://www.boombam.co.il/wp-content/uploads/2023/01/FHAW106810-Fixtec-600x723.png", price: 65 },
-        { id: "t4", name: "מסור חשמלי", description: "מסור חשמלי לחיתוך עץ ומתכת.", image: "https://d3m9l0v76dty0.cloudfront.net/system/photos/12677192/large/ec45040bf9ca9312ef41d8f7f8770e1f.png", price: 450 },
-        { id: "t5", name: "דיסקית חיתוך יהלום", description: "דיסקית איכותית לחיתוך בטון ואבן.", image: "https://www.boombam.co.il/wp-content/uploads/2023/03/%D7%93%D7%99%D7%A1%D7%A7-%D7%97%D7%99%D7%AA%D7%95%D7%9A-CGW-9.jpg", price: 80 },
-        { id: "t6", name: "מברגה חשמלית", description: "מברגה נטענת לעבודות הרכבה ופירוק.", image: "https://img.kwcdn.com/product/fancy/ffb19af5-f9be-4d7d-9bcd-10799fcf1336.jpg?imageView2/2/w/800/q/70/format/webp", price: 210 },
-        { id: "t7", name: "מפלס לייזר", description: "מכשיר מפלס לייזר מקצועי לעבודות דיוק.", image: "https://www.toolmen.co.il/wp-content/uploads/2021/02/laser-level.jpg", price: 600 },
-        { id: "t8", name: "מריצה", description: "מריצה חזקה לעבודות גינה ובנייה.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/wheelbarrow.jpg", price: 180 }
+        { id: "t1", name: "פטיש 5 ק\"ג", description: "פטיש כבד לעבודות הריסה ובנייה.", image: "https://zurmarket.co.il/cdn/shop/files/51fLQVbi5EL._AC_SL1500.jpg?v=1749121553&width=1946" },
+        { id: "t2", name: "מקדחה רוטטת", description: "מקדחה מקצועית לקידוח בבטון ועץ.", image: "https://d3m9l0v76dty0.cloudfront.net/system/photos/11268082/large/44ba92ae69c8e38578d169a71a247471.webp" },
+        { id: "t3", name: "מפתח שוודי", description: "מפתח שוודי מתכוונן בגדלים שונים.", image: "https://www.boombam.co.il/wp-content/uploads/2023/01/FHAW106810-Fixtec-600x723.png" },
+        { id: "t4", name: "מסור חשמלי", description: "מסור חשמלי לחיתוך עץ ומתכת.", image: "https://d3m9l0v76dty0.cloudfront.net/system/photos/12677192/large/ec45040bf9ca9312ef41d8f7f8770e1f.png" },
+        { id: "t5", name: "דיסקית חיתוך יהלום", description: "דיסקית איכותית לחיתוך בטון ואבן.", image: "https://www.boombam.co.il/wp-content/uploads/2023/03/%D7%93%D7%99%D7%A1%D7%A7-%D7%97%D7%99%D7%AA%D7%95%D7%9A-CGW-9.jpg" },
+        { id: "t6", name: "מברגה חשמלית", description: "מברגה נטענת לעבודות הרכבה ופירוק.", image: "https://img.kwcdn.com/product/fancy/ffb19af5-f9be-4d7d-9bcd-10799fcf1336.jpg?imageView2/2/w/800/q/70/format/webp" },
+        { id: "t7", name: "מפלס לייזר", description: "מכשיר מפלס לייזר מקצועי לעבודות דיוק.", image: "https://www.toolmen.co.il/wp-content/uploads/2021/02/laser-level.jpg" },
+        { id: "t8", name: "מריצה", description: "מריצה חזקה לעבודות גינה ובנייה.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/wheelbarrow.jpg" }
     ],
     "מחלקת צבע": [
-        { id: "p1", name: "צבע אקרילי לבן 5 ליטר", description: "צבע אקרילי איכותי לקירות פנים.", image: "https://www.tambour.co.il/wp-content/uploads/2020/03/supercryl-3000.png", price: 110 },
-        { id: "p2", name: "פריימר לקירות 1 ליטר", description: "חומר מקשר לפני צביעה לשיפור אחיזה.", image: "https://www.tambour.co.il/wp-content/uploads/2020/03/primer-s.png", price: 70 },
-        { id: "p3", name: "רולר צבע מקצועי", description: "רולר איכותי לצביעה אחידה ומהירה.", image: "https://www.paintcenter.co.il/wp-content/uploads/2020/03/roller.jpg", price: 25 },
-        { id: "p4", name: "מגש צבע", description: "מגש פלסטיק נוח לשימוש עם רולר.", image: "https://www.paintcenter.co.il/wp-content/uploads/2020/03/paint-tray.jpg", price: 15 },
-        { id: "p5", name: "שליכט צבעוני 20 ק\"ג", description: "שליכט אקרילי גמיש במגוון צבעים.", image: "https://www.tambour.co.il/wp-content/uploads/2020/03/color-plaster.png", price: 250 },
-        { id: "p6", name: "צבע טמבור קירות 18 ליטר", description: "צבע איכותי של טמבור לקירות פנים וחוץ.", image: "https://www.tambour.co.il/wp-content/uploads/2020/03/tambour-wall-paint.png", price: 380 },
-        { id: "p7", name: "צבע אפוקסי לרצפה 1 ליטר", description: "צבע אפוקסי עמיד במיוחד לרצפות תעשייתיות.", image: "https://www.tambour.co.il/wp-content/uploads/2020/03/epoxy-floor-paint.png", price: 190 },
-        { id: "p8", name: "ספריי צבע שחור", description: "צבע ספריי מהיר ייבוש למתכת ועץ.", image: "https://www.tambour.co.il/wp-content/uploads/2020/03/black-spray-paint.png", price: 30 }
+        { id: "p1", name: "צבע אקרילי לבן 5 ליטר", description: "צבע אקרילי איכותי לקירות פנים.", image: "https://www.tambour.co.il/wp-content/uploads/2020/03/supercryl-3000.png" },
+        { id: "p2", name: "פריימר לקירות 1 ליטר", description: "חומר מקשר לפני צביעה לשיפור אחיזה.", image: "https://www.tambour.co.il/wp-content/uploads/2020/03/primer-s.png" },
+        { id: "p3", name: "רולר צבע מקצועי", description: "רולר איכותי לצביעה אחידה ומהירה.", image: "https://www.paintcenter.co.il/wp-content/uploads/2020/03/roller.jpg" },
+        { id: "p4", name: "מגש צבע", description: "מגש פלסטיק נוח לשימוש עם רולר.", image: "https://www.paintcenter.co.il/wp-content/uploads/2020/03/paint-tray.jpg" },
+        { id: "p5", name: "שליכט צבעוני 20 ק\"ג", description: "שליכט אקרילי גמיש במגוון צבעים.", image: "https://www.tambour.co.il/wp-content/uploads/2020/03/color-plaster.png" },
+        { id: "p6", name: "צבע טמבור קירות 18 ליטר", description: "צבע איכותי של טמבור לקירות פנים וחוץ.", image: "https://www.tambour.co.il/wp-content/uploads/2020/03/tambour-wall-paint.png" },
+        { id: "p7", name: "צבע אפוקסי לרצפה 1 ליטר", description: "צבע אפוקסי עמיד במיוחד לרצפות תעשייתיות.", image: "https://www.tambour.co.il/wp-content/uploads/2020/03/epoxy-floor-paint.png" },
+        { id: "p8", name: "ספריי צבע שחור", description: "צבע ספריי מהיר ייבוש למתכת ועץ.", image: "https://www.tambour.co.il/wp-content/uploads/2020/03/black-spray-paint.png" }
     ],
     "ציוד בטיחות": [
-        { id: "s1", name: "קסדת בטיחות", description: "קסדה קשיחה להגנה על הראש באתרי בנייה.", image: "https://www.safety.co.il/wp-content/uploads/2020/03/safety-helmet.jpg", price: 80 },
-        { id: "s2", name: "כפפות עבודה", description: "כפפות עמידות להגנה על הידיים.", image: "https://www.safety.co.il/wp-content/uploads/2020/03/work-gloves.jpg", price: 40 },
-        { id: "s3", name: "נעלי בטיחות", description: "נעליים עם כיפת מגן להגנה על כפות הרגליים.", image: "https://www.safety.co.il/wp-content/uploads/2020/03/safety-shoes.jpg", price: 250 },
-        { id: "s4", name: "אפוד זוהר", description: "אפוד זוהר לנראות גבוהה בשטח.", image: "https://www.safety.co.il/wp-content/uploads/2020/03/reflective-vest.jpg", price: 20 },
-        { id: "s5", name: "משקפי מגן", description: "משקפי מגן לעיניים מפני אבק ורסיסים.", image: "https://www.safety.co.il/wp-content/uploads/2020/03/safety-glasses.jpg", price: 30 },
-        { id: "s6", name: "אוזניות מגן", description: "אוזניות להגנה מפני רעש חזק.", image: "https://www.safety.co.il/wp-content/uploads/2020/03/ear-protection.jpg", price: 70 }
+        { id: "s1", name: "קסדת בטיחות", description: "קסדה קשיחה להגנה על הראש באתרי בנייה.", image: "https://www.safety.co.il/wp-content/uploads/2020/03/safety-helmet.jpg" },
+        { id: "s2", name: "כפפות עבודה", description: "כפפות עמידות להגנה על הידיים.", image: "https://www.safety.co.il/wp-content/uploads/2020/03/work-gloves.jpg" },
+        { id: "s3", name: "נעלי בטיחות", description: "נעליים עם כיפת מגן להגנה על כפות הרגליים.", image: "https://www.safety.co.il/wp-content/uploads/2020/03/safety-shoes.jpg" },
+        { id: "s4", name: "אפוד זוהר", description: "אפוד זוהר לנראות גבוהה בשטח.", image: "https://www.safety.co.il/wp-content/uploads/2020/03/reflective-vest.jpg" },
+        { id: "s5", name: "משקפי מגן", description: "משקפי מגן לעיניים מפני אבק ורסיסים.", image: "https://www.safety.co.il/wp-content/uploads/2020/03/safety-glasses.jpg" },
+        { id: "s6", name: "אוזניות מגן", description: "אוזניות להגנה מפני רעש חזק.", image: "https://www.safety.co.il/wp-content/uploads/2020/03/ear-protection.jpg" }
     ],
     "מחלקת גינה": [
-        { id: "g1", name: "מכסחת דשא חשמלית", description: "מכסחת דשא חשמלית קלה ונוחה לשימוש.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/lawn-mower.jpg", price: 600 },
-        { id: "g2", name: "צינור השקיה 20 מ'", description: "צינור השקיה גמיש ועמיד לגינה.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/garden-hose.jpg", price: 90 },
-        { id: "g3", name: "מזמרה מקצועית", description: "מזמרה חדה ואיכותית לגיזום עצים ושיחים.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/pruning-shears.jpg", price: 50 },
-        { id: "g4", name: "דשן נוזלי 1 ליטר", description: "דשן נוזלי מרוכז לדישון צמחי גינה.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/liquid-fertilizer.jpg", price: 45 },
-        { id: "g5", name: "מפוח עלים חשמלי", description: "מפוח חשמלי/בנזין לפינוי עלים ופסולת גינה.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/leaf-blower.jpg", price: 300 },
-        { id: "g6", name: "מערכת השקיה בטפטוף", description: "ערכה מלאה להקמת מערכת השקיה חכמה.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/drip-irrigation.jpg", price: 180 }
+        { id: "g1", name: "מכסחת דשא חשמלית", description: "מכסחת דשא חשמלית קלה ונוחה לשימוש.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/lawn-mower.jpg" },
+        { id: "g2", name: "צינור השקיה 20 מ'", description: "צינור השקיה גמיש ועמיד לגינה.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/garden-hose.jpg" },
+        { id: "g3", name: "מזמרה מקצועית", description: "מזמרה חדה ואיכותית לגיזום עצים ושיחים.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/pruning-shears.jpg" },
+        { id: "g4", name: "דשן נוזלי 1 ליטר", description: "דשן נוזלי מרוכז לדישון צמחי גינה.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/liquid-fertilizer.jpg" },
+        { id: "g5", name: "מפוח עלים חשמלי", description: "מפוח חשמלי/בנזין לפינוי עלים ופסולת גינה.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/leaf-blower.jpg" },
+        { id: "g6", name: "מערכת השקיה בטפטוף", description: "ערכה מלאה להקמת מערכת השקיה חכמה.", image: "https://www.gardenya.co.il/wp-content/uploads/2020/03/drip-irrigation.jpg" }
     ],
     "שירותי הובלה": [
-        { id: "h1", name: "הובלת מלט (טון)", description: "הובלת מנוף עד 10 מטר לאתר הלקוח.", image: "https://placehold.co/120x120/f0f0f0/333333?text=הובלת+מלט", price: 200 },
-        { id: "h2", name: "מנוף 20 מטר", description: "שירות מנוף עד גובה 20 מטר להרמת חומרים.", image: "https://placehold.co/120x120/f0f0f0/333333?text=מנוף", price: 800 },
-        { id: "h3", name: "הצבת מכולה פסולת 8 קוב", description: "הצבה מכולת פסולת בניין 8 קוב.", image: "https://uploads.homeless.co.il/yad2/202202/nvFile4157549.jpg", price: 400 },
-        { id: "h4", name: "החלפה מכולה פסולת 8 קוב", description: "החלפה מכולת פסולת בניין 8 קוב.", image: "https://uploads.homeless.co.il/yad2/202202/nvFile4157549.jpg", price: 350 },
-        { id: "h5", name: "הוצאה מכולה פסולת 8 קוב", description: "הוצאה מכולת פסולת בניין 8 קוב.", image: "https://uploads.homeless.co.il/yad2/202202/nvFile4157549.jpg", price: 300 }
+        { id: "h1", name: "הובלת מלט (טון)", description: "הובלת מנוף עד 10 מטר לאתר הלקוח.", image: "https://placehold.co/120x120/f0f0f0/333333?text=הובלת+מלט" },
+        { id: "h2", name: "מנוף 20 מטר", description: "שירות מנוף עד גובה 20 מטר להרמת חומרים.", image: "https://placehold.co/120x120/f0f0f0/333333?text=מנוף" },
+        { id: "h3", name: "הצבת מכולה פסולת 8 קוב", description: "הצבה מכולת פסולת בניין 8 קוב.", image: "https://uploads.homeless.co.il/yad2/202202/nvFile4157549.jpg" },
+        { id: "h4", name: "החלפה מכולה פסולת 8 קוב", description: "החלפה מכולת פסולת בניין 8 קוב.", image: "https://uploads.homeless.co.il/yad2/202202/nvFile4157549.jpg" },
+        { id: "h5", name: "הוצאה מכולה פסולת 8 קוב", description: "הוצאה מכולת פסולת בניין 8 קוב.", image: "https://uploads.homeless.co.il/yad2/202202/nvFile4157549.jpg" }
     ]
 };
 
